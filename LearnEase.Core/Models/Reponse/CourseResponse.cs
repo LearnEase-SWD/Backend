e7fc0774e19cd951183d6ed5bd9210f4deb75916
tranducvuong03 <tranducvuong31012003@gmail.com>
2025-03-28 20:37:27 +0700

    Update CourseResponse

--- LearnEase.Core/Models/Reponse/CourseResponse.cs
+++ LearnEase.Core/Models/Reponse/CourseResponse.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LearnEase.Core.Models.Reponse
+﻿namespace LearnEase.Core.Models.Reponse
 {
     public class CourseResponse
     {
         public Guid CourseID { get; set; }
+		public Guid TopicId { get; set; }
 		public string TopicName { get; set; }
 		public string Title { get; set; }
         public decimal Price { get; set; }
@@ -21,5 +16,4 @@ namespace LearnEase.Core.Models.Reponse
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
     }
-
 }
