beeb291af5808e29b0de176a291fb947f363574c
khoang1205 <khoandse172345@fpt.edu.vn>
2025-03-29 10:36:22 +0700

    fix progress

--- LearnEase.Core/Entities/UserLesson.cs
+++ LearnEase.Core/Entities/UserLesson.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 namespace LearnEase.Core.Entities
 {
     public class UserLesson
-	{
+    {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid ProgressID { get; set; }
@@ -18,9 +18,16 @@ namespace LearnEase.Core.Entities
         public Lesson Lesson { get; set; }
 
         [Required]
-		[Range(0, 100)]
-		public int Progress { get; set; }  // 0% , 20%, 50%, ...
+        [Range(0, 100)]
+        public int Progress { get; set; }  // 0% , 20%, 50%, ...
 
+        public bool IsVideoCompleted { get; set; } = false; // Đã xem xong video chưa?
+        public bool IsExerciseCompleted { get; set; } = false; // Đã làm xong bài tập chưa?
+        public bool HasAccessedFlashcards { get; set; } = false;
+        public bool IsTheoryCompleted { get; set; } = false; // Đã đọc xong lý thuyết chưa?
+
+        public DateTime StartedAt { get; set; } = DateTime.UtcNow; // Thời điểm bắt đầu Lesson
+        public DateTime? CompletedAt { get; set; } // Thời điểm hoàn thành Lesson
         public DateTime LastAccessedAt { get; set; } = DateTime.UtcNow;
     }
 }
